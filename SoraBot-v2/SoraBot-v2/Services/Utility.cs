ef66c1dd1235fb8cc1c5830eb7a4268d4fe490e0
Serenity <d.palbanese@gmail.com>
2017-09-15 19:37:48 +0200

    updated version

--- SoraBot-v2/SoraBot-v2/Services/Utility.cs
+++ SoraBot-v2/SoraBot-v2/Services/Utility.cs
@@ -25,7 +25,7 @@ namespace SoraBot_v2.Services
         public static Discord.Color BlueInfoEmbed = new Discord.Color(59,136,195);
         public static string StandardDiscordAvatar = "http://i.imgur.com/tcpgezi.jpg";
 
-        public const string SORA_VERSION = "2.0.0-beta.3";
+        public const string SORA_VERSION = "2.0.0-beta.4";
         
         public const string SORA_ADMIN_ROLE_NAME = "Sora-Admin";
         public const string SORA_DJ_ROLE_NAME = "Sora-DJ";
