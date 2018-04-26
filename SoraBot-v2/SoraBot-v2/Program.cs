db95946ae9372ae3bd3a95793d52102dbf3e1bb4
Serenity <d.palbanese@gmail.com>
2018-04-26 13:53:24 +0200

    added a pretty logger :>

--- SoraBot-v2/SoraBot-v2/Program.cs
+++ SoraBot-v2/SoraBot-v2/Program.cs
@@ -111,7 +111,7 @@ namespace SoraBot_v2
             await _client.StartAsync();
 
             // initialize Autoreconnect Feature
-            _autoReconnectService = new AutoReconnectService(_client, token);
+            _autoReconnectService = new AutoReconnectService(_client, LogPretty);
             //build webserver and inject service
             try
             {
@@ -186,6 +186,39 @@ namespace SoraBot_v2
 
             return new DefaultServiceProviderFactory().CreateServiceProvider(services);
         }
+
+        // Example of a logging handler. This can be re-used by addons
+    // that ask for a Func<LogMessage, Task>.
+    private static Task LogPretty(LogMessage message)
+    {
+        switch (message.Severity)
+        {
+            case LogSeverity.Critical:
+            case LogSeverity.Error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case LogSeverity.Warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogSeverity.Info:
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case LogSeverity.Verbose:
+            case LogSeverity.Debug:
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                break;
+        }
+        Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+        Console.ResetColor();
+        
+        // If you get an error saying 'CompletedTask' doesn't exist,
+        // your project is targeting .NET 4.5.2 or lower. You'll need
+        // to adjust your project's target framework to 4.6 or higher
+        // (instructions for this are easily Googled).
+        // If you *need* to run on .NET 4.5 for compat/other reasons,
+        // the alternative is to 'return Task.Delay(0);' instead.
+        return Task.CompletedTask;
+    }
 
         private Task Log(LogMessage m)
         {
--- SoraBot-v2/SoraBot-v2/Services/AutoReconnectService.cs
+++ SoraBot-v2/SoraBot-v2/Services/AutoReconnectService.cs
@@ -2,66 +2,106 @@ using Discord.WebSocket;
 using Discord;
 using System.Threading.Tasks;
 using System;
+using System.Threading;
 
 namespace SoraBot_v2.Services
 {
     public class AutoReconnectService
     {
-        private DiscordSocketClient _client;
-        private readonly TimeSpan _timeout = TimeSpan.FromSeconds(30);
-        private string _token;
+       // --- Begin Configuration Section ---
+        // How long should we wait on the client to reconnect before resetting?
+        private static readonly TimeSpan _timeout = TimeSpan.FromSeconds(30);
 
-        public AutoReconnectService(DiscordSocketClient client, string token)
+        // Should we attempt to reset the client? Set this to false if your client is still locking up.
+        private static readonly bool _attemptReset = true;
+
+        // Change log levels if desired:
+        private static readonly LogSeverity _debug = LogSeverity.Debug;
+        private static readonly LogSeverity _info = LogSeverity.Info;
+        private static readonly LogSeverity _critical = LogSeverity.Critical;
+        // --- End Configuration Section ---
+
+        private readonly DiscordSocketClient _discord;
+        private readonly Func<LogMessage, Task> _logger;
+        private CancellationTokenSource _cts;        
+
+        public AutoReconnectService(DiscordSocketClient discord, Func<LogMessage, Task> logger = null)
         {
-            _client = client;
-            _token = token;
-            //start reconnection loop
-            Task.Factory.StartNew(()=>{ AutoReconnect(); }, TaskCreationOptions.LongRunning);
+            _cts = new CancellationTokenSource();
+            _discord = discord;
+            _logger = logger ?? (_ => Task.CompletedTask);
+
+            _discord.Connected += ConnectedAsync;
+            _discord.Disconnected += DisconnectedAsync;
         }
 
-        private async Task AutoReconnect(){
-            while(true){
-                if(_client.ConnectionState == ConnectionState.Disconnected){
-                    //this could happen while discord.net is still trying to auto reconnect. 
-                    //Thus lets wait for a minute and see if this changed.
-                    await Task.Delay(60000);
-                    //Now recheck the condition
-                    if(_client.ConnectionState != ConnectionState.Disconnected) { 
-                        //To prevent further indention                        
-                        continue;
-                    }
-                    //Start reconnection process
-                    await Reconnect();
+        public Task ConnectedAsync()
+        {
+            // Cancel all previous state checks and reset the CancelToken - client is back online
+            _ = DebugAsync("Client reconnected, resetting cancel tokens...");
+            _cts.Cancel();
+            _cts = new CancellationTokenSource();
+            _ = DebugAsync("Client reconnected, cancel tokens reset.");
+
+            return Task.CompletedTask;
+        }
+
+        public Task DisconnectedAsync(Exception _e)
+        {
+            // Check the state after <timeout> to see if we reconnected
+            _ = InfoAsync("Client disconnected, starting timeout task...");
+            _ = Task.Delay(_timeout, _cts.Token).ContinueWith(async _ => 
+            {
+                await DebugAsync("Timeout expired, continuing to check client state...");
+                await CheckStateAsync();
+                await DebugAsync("State came back okay");
+            });
+
+            return Task.CompletedTask;
+        }
+
+        private async Task CheckStateAsync()
+        {
+            // Client reconnected, no need to reset
+            if (_discord.ConnectionState == ConnectionState.Connected) return;
+            if (_attemptReset)
+            {
+                await InfoAsync("Attempting to reset the client");
+
+                var timeout = Task.Delay(_timeout);
+                var connect = _discord.StartAsync();
+                var task = await Task.WhenAny(timeout, connect);
+
+                if (task == timeout)
+                {
+                    await CriticalAsync("Client reset timed out (task deadlocked?), killing process");
+                    FailFast();
                 }
-                //wait with check again.
-                await Task.Delay(60000);
-            }
-        }
-
-        private async Task Reconnect(){
-            //reconnect
-            var timeout = Task.Delay(_timeout);
-            var connect = _client.LoginAsync(TokenType.Bot, _token);
-            var task = await Task.WhenAny(timeout, connect);
-
-             if (task == timeout){
-                Console.WriteLine("Client reset timed out. Retrying...");
-            }
-            else if (connect.IsFaulted){
-                Console.WriteLine("Client login failed. Retrying...", connect.Exception);
-            }
-            else if (connect.IsCompletedSuccessfully){
-                //run start
-                try{
-                    await _client.StartAsync();
-                } catch(Exception e){
-                    Console.WriteLine("Client start failed. Retrying...\n"+e.ToString());
-                    return;
+                else if (connect.IsFaulted)
+                {
+                    await CriticalAsync("Client reset faulted, killing process", connect.Exception);
+                    FailFast();
                 }
-                Console.WriteLine("Reconnect Successfull...");
+                else if (connect.IsCompletedSuccessfully)
+                    await InfoAsync("Client reset succesfully!");
+                return;
             }
 
+            await CriticalAsync("Client did not reconnect in time, killing process");
+            FailFast();
         }
+
+        private void FailFast()
+            => Environment.Exit(1);
+
+        // Logging Helpers
+        private const string LogSource = "Reliability";
+        private Task DebugAsync(string message)
+            => _logger.Invoke(new LogMessage(_debug, LogSource, message));
+        private Task InfoAsync(string message)
+            => _logger.Invoke(new LogMessage(_info, LogSource, message));
+        private Task CriticalAsync(string message, Exception error = null)
+            => _logger.Invoke(new LogMessage(_critical, LogSource, message, error));
 
     }
 }
