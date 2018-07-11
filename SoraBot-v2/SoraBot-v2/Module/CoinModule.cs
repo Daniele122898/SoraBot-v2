523448e79c23c350641723aff608e2f29d8d45fc
Serenity <d.palbanese@gmail.com>
2018-07-11 22:25:59 +0200

    add get daily

---
+++ SoraBot-v2/SoraBot-v2/Module/CoinModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Humanizer;
+using Humanizer.Localisation;
+using SoraBot_v2.Data;
+using SoraBot_v2.Services;
+
+namespace SoraBot_v2.Module
+{
+    public class CoinModule : ModuleBase<SocketCommandContext>
+    {
+        private CoinService _coinService;
+
+        public CoinModule(CoinService coinService)
+        {
+            _coinService = coinService;
+        }
+        
+        // Gain coins
+        [Command("daily"), Alias("earn", "dailies"), Summary("Gives you a daily reward of Sora Coins")]
+        public async Task GetDaily()
+        {
+            await _coinService.DoDaily(Context);
+        }
+        
+        // TODO give coins
+        
+        // TODO check coins
+        
+    }
+}
---
+++ SoraBot-v2/SoraBot-v2/Services/CoinService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Humanizer;
+using Humanizer.Localisation;
+using Microsoft.AspNetCore.Hosting.Internal;
+using SoraBot_v2.Data;
+
+namespace SoraBot_v2.Services
+{
+    public class CoinService
+    {
+        private const int GAIN_COINS = 500;
+
+        public async Task DoDaily(SocketCommandContext context )
+        {
+            using (var soraContext = new SoraContext())
+            {
+                // get user db data
+                var userdb = Utility.GetOrCreateUser(context.User.Id, soraContext);
+                // Check if he can gain again or userdb is null for some odd reason
+                if (userdb == null)
+                {
+                    await context.Channel.SendMessageAsync("",
+                        embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
+                            "Something went wrong soryy :c"));
+                    return;
+                }
+                if (userdb.NextDaily.CompareTo(DateTime.UtcNow) >= 0)
+                {
+                    var timeRemaining = userdb.NextDaily.Subtract(DateTime.UtcNow.TimeOfDay).TimeOfDay;
+                    await context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(Utility.RedFailiureEmbed,
+                        Utility.SuccessLevelEmoji[2],
+                        $"You can't earn anymore, please wait another {timeRemaining.Humanize(minUnit: TimeUnit.Second)}!"));
+                    return;
+                }
+                // add 24h cooldown
+                userdb.NextDaily = DateTime.UtcNow.AddHours(24);
+                // give coins
+                userdb.Money += GAIN_COINS;
+                // save changes
+                await soraContext.SaveChangesAsync();
+
+                await context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(
+                    Utility.GreenSuccessEmbed,
+                    Utility.SuccessLevelEmoji[0],
+                    $"You gained {GAIN_COINS} Sora Coins! You can earn again in 24h."));
+            }       
+        }
+    }
+}
