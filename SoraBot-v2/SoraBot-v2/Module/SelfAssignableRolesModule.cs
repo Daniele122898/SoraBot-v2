ed2a1fcf07efed18f4616c1a6e6c3f814a6d27d7
Serenity <d.palbanese@gmail.com>
2018-07-14 23:17:31 +0200

    added parsing of time

--- SoraBot-v2/SoraBot-v2/Module/SelfAssignableRolesModule.cs
+++ SoraBot-v2/SoraBot-v2/Module/SelfAssignableRolesModule.cs
@@ -17,23 +17,39 @@ namespace SoraBot_v2.Module
 
         [Command("addsar"), Alias("asar", "addrole"),
          Summary("Adds a self assignable role to the list. If it doesn't exist sora will create it")]
-        public async Task AddSar([Remainder] string roleName)
-        {
-            await _sarService.AddSarToList(Context, roleName.Trim()); //TODO EXPAND THIS
-        }
-
-        [Command("defaultrole"), Alias("drole", "default"), Summary("Sets a default role for when users join")]
-        public async Task AddDefaultRole([Remainder] string roleName, int cost = 0, [Remainder] string expires = null)
+        public async Task AddSar([Remainder] string roleName, int cost = 0, [Remainder] string expires = null)
         {
             if (cost == 0)
             {
-                await _sarService.AddDefaultRole(Context, roleName.Trim());
-                return;
+                await _sarService.AddSarToList(Context, roleName.Trim()); //TODO EXPAND THIS
+            }
+
+            if (string.IsNullOrWhiteSpace(expires))
+            {
+                await _sarService.AddSarToList(Context, roleName.Trim(), false, cost); 
             }
             
             // parse time
+            var time = GetTime(expires);
+            // check if time was valid
+            if (time == null)
+            {
+                await ReplyAsync("",
+                    embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
+                        "Failed to parse time for expiration"));
+                return;
+            }
+            var timeSpan = new TimeSpan(time[0], time[1], time[2], time[3]);
+            await _sarService.AddSarToList(Context, roleName.Trim(), true, cost, timeSpan);
+        }
+
+        [Command("defaultrole"), Alias("drole", "default"), Summary("Sets a default role for when users join")]
+        public async Task AddDefaultRole([Remainder] string roleName)
+        {
             
-            
+                await _sarService.AddDefaultRole(Context, roleName.Trim());
+                return;
+
         }
 
         [Command("toggledefault"), Alias("toggledef"), Summary("Toggles if default role is on or off")]
@@ -66,25 +82,25 @@ namespace SoraBot_v2.Module
             await _sarService.ListSars(Context);
         }
 
-        private double GetTime(string msg)
+        private int[] GetTime(string msg)
         {
             var regex = Regex.Matches(msg, @"(\d+)\s{0,1}([a-zA-Z]*)");
-            double timeToAdd = 0;
+            var add = new int[3];
             for (int i = 0; i < regex.Count; i++)
             {
                 var captures = regex[i].Groups;
                 if (captures.Count < 3)
                 {
                     Console.WriteLine("CAPTURES COUNT LESS THEN 3");
-                    return 0;
+                    return null;
                 }
 
-                double amount = 0;
+                int amount = 0;
 
-                if (!Double.TryParse(captures[1].ToString(), out amount))
+                if (!int.TryParse(captures[1].ToString(), out amount))
                 {
                     Console.WriteLine($"COULDNT PARSE DOUBLE : {captures[1].ToString()}");
-                    return 0;
+                    return null;
                 }
 
                 switch (captures[2].ToString())
@@ -92,36 +108,36 @@ namespace SoraBot_v2.Module
                     case ("weeks"):
                     case ("week"):
                     case ("w"):
-                        timeToAdd += amount * 604800;
+                        add[0] += amount * 7;
                         break;
                     case ("day"):
                     case ("days"):
                     case ("d"):
-                        timeToAdd += amount * 86400;
+                        add[0] += amount;
                         break;
                     case ("hours"):
                     case ("hour"):
                     case ("h"):
-                        timeToAdd += amount * 3600;
+                        add[1] += amount;
                         break;
                     case ("minutes"):
                     case ("minute"):
                     case ("m"):
                     case ("min"):
                     case ("mins"):
-                        timeToAdd += amount * 60;
+                        add[2] += amount;
                         break;
                     case ("seconds"):
                     case ("second"):
                     case ("s"):
-                        timeToAdd += amount;
+                        add[3] += amount;
                         break;
                     default:
                         Console.WriteLine("SWITCH FAILED");
-                        return 0;
+                        return null;
                 }
             }
-            return timeToAdd;
+            return add;
         }
         
     }
