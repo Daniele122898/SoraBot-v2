22b4ccc8fa72ab5f767e2fecf460b3a0efb59ae5
Serenity <d.palbanese@gmail.com>
2018-07-27 23:26:57 +0200

    yeah im tired just gonna commit just in case lul

--- SoraBot-v2/SoraBot-v2/Services/WaifuService.cs
+++ SoraBot-v2/SoraBot-v2/Services/WaifuService.cs
@@ -1,9 +1,35 @@
-﻿namespace SoraBot_v2.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SoraBot_v2.Data;
+using SoraBot_v2.Data.Entities.SubEntities;
+
+namespace SoraBot_v2.Services
 {
+    
     public class WaifuService
     {
         // TODO unbox, sell, maybe trade, maybe fav
+        private List<Waifu> _boxCache = new List<Waifu>();
         
+        public void Initialize()
+        {
+            // initial setup
+            CreateRandomCache(null);
+            // setup timer
+        }
+
+        public void CreateRandomCache(Object stateInfo)
+        {
+            using (var soraContext = new SoraContext())
+            {
+                
+            }
+        }
         
+        public async Task UnboxWaifu()
+        {
+            
+        }
     }
 }
