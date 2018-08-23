477ff350bff929a841aa42607dc032b3e1de2c3f
Serenity <d.palbanese@gmail.com>
2018-08-23 14:50:27 +0200

    Added shuffle extension to List type. Create initial box cache and fill it with waifus proportional to their rarity. This seems super inefficient but i cant think of another way to create fair RNG rn

--- SoraBot-v2/SoraBot-v2/Services/WaifuService.cs
+++ SoraBot-v2/SoraBot-v2/Services/WaifuService.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using SoraBot_v2.Data;
 using SoraBot_v2.Data.Entities.SubEntities;
+using SoraBot_v2.Extensions;
 
 namespace SoraBot_v2.Services
 {
+    
+    /*
+        Waifu trade
+        ---------------
+        
+        Rarities
+        ---------
+        common				2000
+        uncommon			800
+        rare				200
+        epic				20
+        ultimate Waifu		1
+        
+        Waifu
+        ---------
+        id
+        name
+        imageurl
+        rarity
+        
+        User Ownership
+        -----------------
+        id increment
+        userid
+        waifuId
+        count
+     */
     
     public class WaifuService
     {
@@ -15,21 +45,50 @@ namespace SoraBot_v2.Services
         public void Initialize()
         {
             // initial setup
-            CreateRandomCache(null);
-            // setup timer
+            CreateRandomCache();
         }
 
-        public void CreateRandomCache(Object stateInfo)
+        private void CreateRandomCache()
         {
             using (var soraContext = new SoraContext())
             {
-                
+                // get all waifus
+                var waifus = soraContext.Waifus.ToArray();
+                foreach (var waifu in waifus)
+                {
+                    // add each waifu * rarity amount to cache
+                    int amount = GetRarityAmount(waifu.Rarity);
+                    for (int i = 0; i < amount; i++)
+                    {
+                        _boxCache.Add(waifu);
+                    }
+                }
+                // shuffle for some extra RNG
+                _boxCache.Shuffle();
             }
         }
         
         public async Task UnboxWaifu()
         {
             
+        }
+
+        private int GetRarityAmount(WaifuRarity rarity)
+        {
+            switch (rarity)
+            {
+                    case WaifuRarity.Common:
+                        return 2000;
+                    case WaifuRarity.Uncommon:
+                        return 800;
+                    case WaifuRarity.Rare:
+                        return 200;
+                    case WaifuRarity.Epic:
+                        return 20;
+                    case WaifuRarity.UltimateWaifu:
+                        return 1;
+            }
+            return 0;
         }
     }
 }
