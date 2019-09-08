3395abb8e44751ab281be2c2955b90e657ceaaae
Serenity <d.palbanese@gmail.com>
2019-09-08 14:10:07 +0200

    added locks to daily. added timeout fail safe

--- SoraBot-v2/SoraBot-v2/Services/CoinService.cs
+++ SoraBot-v2/SoraBot-v2/Services/CoinService.cs
@@ -65,8 +65,21 @@ namespace SoraBot_v2.Services
                 GetSortedLocks(context.User.Id, userId, out var lock1, out var lock2);
                 try
                 {
-                    await lock1.WaitAsync(LOCK_TIMOUT_MSECONDS);
-                    await lock2.WaitAsync(LOCK_TIMOUT_MSECONDS);
+                    if (!await lock1.WaitAsync(LOCK_TIMOUT_MSECONDS))
+                    {
+                        await context.Channel.SendMessageAsync("",
+                            embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
+                                "Something went wrong sorry :c").Build());
+                        return;
+                    }
+
+                    if (!await lock2.WaitAsync(LOCK_TIMOUT_MSECONDS))
+                    {
+                        await context.Channel.SendMessageAsync("",
+                            embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
+                                "Something went wrong sorry :c").Build());
+                        return;
+                    }
                     // get current userDb
                     var userdb = Utility.OnlyGetUser(context.User.Id, soraContext);
                     // check if user exists and if he has the money
@@ -126,35 +139,55 @@ namespace SoraBot_v2.Services
         {
             using (var soraContext = new SoraContext())
             {
-                // get user db data
-                var userdb = Utility.GetOrCreateUser(context.User.Id, soraContext);
-                // Check if he can gain again or userdb is null for some odd reason
-                if (userdb == null)
+                var lck = GetOrCreateLock(context.User.Id);
+                try
                 {
-                    await context.Channel.SendMessageAsync("",
-                        embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
-                            "Something went wrong soryy :c").Build());
-                    return;
-                }
-                if (userdb.NextDaily.CompareTo(DateTime.UtcNow) >= 0)
-                {
-                    var timeRemaining = userdb.NextDaily.Subtract(DateTime.UtcNow.TimeOfDay).TimeOfDay;
-                    await context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(Utility.RedFailiureEmbed,
-                        Utility.SuccessLevelEmoji[2],
-                        $"You can't earn anymore, please wait another {timeRemaining.Humanize(minUnit: TimeUnit.Second, precision: 2)}!").Build());
-                    return;
-                }
-                // add 20h cooldown
-                userdb.NextDaily = DateTime.UtcNow.AddHours(DAILY_COOLDOWN);
-                // give coins
-                userdb.Money += GAIN_COINS;
-                // save changes
-                await soraContext.SaveChangesAsync();
+                    if (!await lck.WaitAsync(LOCK_TIMOUT_MSECONDS))
+                    {
+                        await context.Channel.SendMessageAsync("",
+                            embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
+                                "Something went wrong sorry :c").Build());
+                        return;
+                    }
+                    // get user db data
+                    var userdb = Utility.GetOrCreateUser(context.User.Id, soraContext);
+                    // Check if he can gain again or userdb is null for some odd reason
+                    if (userdb == null)
+                    {
+                        await context.Channel.SendMessageAsync("",
+                            embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.SuccessLevelEmoji[2],
+                                "Something went wrong sorry :c").Build());
+                        return;
+                    }
 
-                await context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(
-                    Utility.GreenSuccessEmbed,
-                    Utility.SuccessLevelEmoji[0],
-                    $"You gained {GAIN_COINS} Sora Coins! You can earn again in {DAILY_COOLDOWN}h.").Build());
+                    if (userdb.NextDaily.CompareTo(DateTime.UtcNow) >= 0)
+                    {
+                        var timeRemaining = userdb.NextDaily.Subtract(DateTime.UtcNow.TimeOfDay).TimeOfDay;
+                        await context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(
+                                Utility.RedFailiureEmbed,
+                                Utility.SuccessLevelEmoji[2],
+                                $"You can't earn anymore, please wait another {timeRemaining.Humanize(minUnit: TimeUnit.Second, precision: 2)}!")
+                            .Build());
+                        return;
+                    }
+
+                    // add 20h cooldown
+                    userdb.NextDaily = DateTime.UtcNow.AddHours(DAILY_COOLDOWN);
+                    // give coins
+                    userdb.Money += GAIN_COINS;
+                    // save changes
+                    await soraContext.SaveChangesAsync();
+
+                    await context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(
+                        Utility.GreenSuccessEmbed,
+                        Utility.SuccessLevelEmoji[0],
+                        $"You gained {GAIN_COINS} Sora Coins! You can earn again in {DAILY_COOLDOWN}h.").Build());
+                }
+                
+                finally
+                {
+                    lck.Release();
+                }
             }       
         }
 
