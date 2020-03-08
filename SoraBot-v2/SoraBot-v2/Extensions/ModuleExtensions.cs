6f55d31ef79799bc81ba3b76ed3f822d63c2bdb7
Serenity <d.palbanese@gmail.com>
2020-03-08 10:38:38 +0100

    Cherry picked Module Extension

--- SoraBot-v2/SoraBot-v2/Extensions/ModuleExtensions.cs
+++ SoraBot-v2/SoraBot-v2/Extensions/ModuleExtensions.cs
@@ -11,5 +11,20 @@ namespace SoraBot_v2.Extensions
         {
             return await module.Context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(embedColor, embedEmoji, message).Build());
         }
+        
+        public static async Task<IUserMessage> ReplySoraEmbedFailiureResponse(this ModuleBase<SocketCommandContext> module, string message)
+        {
+            return await module.Context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(Utility.RedFailiureEmbed, Utility.FailiureEmoji, message).Build());
+        }
+        
+        public static async Task<IUserMessage> ReplySoraEmbedWarningResponse(this ModuleBase<SocketCommandContext> module, string message)
+        {
+            return await module.Context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(Utility.YellowWarningEmbed, Utility.WarnEmoji, message).Build());
+        }
+        
+        public static async Task<IUserMessage> ReplySoraEmbedSuccessResponse(this ModuleBase<SocketCommandContext> module, string message)
+        {
+            return await module.Context.Channel.SendMessageAsync("", embed: Utility.ResultFeedback(Utility.GreenSuccessEmbed, Utility.SuccessEmoji, message).Build());
+        }
     }
 }
