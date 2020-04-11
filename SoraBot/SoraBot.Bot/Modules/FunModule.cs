b4f387fa7b5b728a53f94dea9c36c233b3186755
Serenity <d.palbanese@gmail.com>
2020-04-12 00:11:17 +0200

    Added fun module

--- SoraBot/SoraBot.Bot/Modules/FunModule.cs
+++ SoraBot/SoraBot.Bot/Modules/FunModule.cs
@@ -1,7 +1,23 @@
-﻿namespace SoraBot.Bot.Modules
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using SoraBot.Common.Extensions.Modules;
+
+namespace SoraBot.Bot.Modules
 {
-    public class FunModule
+    [Group("Fun")]
+    [Name("Fun")]
+    [Summary("A bunch of fun and useless commands.")]
+    public class FunModule : SoraSocketCommandModule
     {
-        
+        [Command("ruined")]
+        [Alias("dayruined")]
+        [Summary("Posts the meme my disappointment is immeasurable")]
+        public Task DayRuined() =>
+            ReplyAsync("", embed: new EmbedBuilder()
+            {
+                ImageUrl = "https://i.imgur.com/pIddxrw.png",
+                Color = Purple
+            }.Build());
     }
 }
