309762a861427a36f926cba12be1c47f9e9ee2c6
Serenity <d.palbanese@gmail.com>
2020-04-11 14:45:28 +0200

    Added Swagger to the API

---
+++ SoraBot/SoraBot.Bot/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SoraBot.Bot.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSoraBot(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
--- SoraBot/SoraBot.WebApi/Startup.cs
+++ SoraBot/SoraBot.WebApi/Startup.cs
@@ -1,12 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using SoraBot.Bot.Extensions;
 
 namespace SoraBot.WebApi
 {
     public class Startup
     {
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "SoraBot API",
+                    Version = "v1",
+                    Description = "SoraBot API"
+                });
+                
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                
+            });
+            
+            this.ConfigureServices(services);
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -15,6 +42,8 @@ namespace SoraBot.WebApi
             services.AddRouting(op => op.LowercaseUrls = true);
             
             services.AddCors();
+
+            services.AddSoraBot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,6 +52,12 @@ namespace SoraBot.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SoraBot API");
+                });
             }
             
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
