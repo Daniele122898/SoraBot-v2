f17ad669a8b401c9ce7dfb68b92ae93a60d8c440
Serenity <d.palbanese@gmail.com>
2020-04-12 21:33:23 +0200

    Added 8ball

--- SoraBot/SoraBot.Bot/Modules/FunModule.cs
+++ SoraBot/SoraBot.Bot/Modules/FunModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using SoraBot.Common.Extensions.Modules;
@@ -9,8 +10,7 @@ namespace SoraBot.Bot.Modules
     [Summary("A bunch of fun and useless commands.")]
     public class FunModule : SoraSocketCommandModule
     {
-        [Command("ruined")]
-        [Alias("dayruined")]
+        [Command("ruined"), Alias("dayruined")]
         [Summary("Posts the meme my disappointment is immeasurable")]
         public Task DayRuined() =>
             ReplyAsync("", embed: new EmbedBuilder()
@@ -26,5 +26,39 @@ namespace SoraBot.Bot.Modules
             await Task.Delay(1500);
             await msg.ModifyAsync(x => { x.Content = "( ͡⌐■ ͜ʖ ͡-■)"; });
         }
+        
+        [Command("8ball"), Alias("8b"), Summary("Ask and get an 8ball answer")]
+        public async Task Ball([Summary("Question"), Remainder] string question)
+        {
+            Random r = new Random();
+            await ReplyDefaultEmbed("🎱 " + ball[r.Next(ball.Length)]);
+        }
+
+        #region data
+        private string[] ball = new[]
+        {
+            "Signs point to yes. ",
+            "Yes.",
+            "Reply hazy, try again.",
+            "Without a doubt. ",
+            "My sources say no. ",
+            "As I see it, yes. ",
+            "You may rely on it.",
+            "Concentrate and ask again.",
+            "Outlook not so good. ",
+            "It is decidedly so.",
+            "Better not tell you now.",
+            "Very doubtful. ",
+            "Yes - definitely. ",
+            "It is certain. ",
+            "Cannot predict now. ",
+            "Most likely. ",
+            "Ask again later. ",
+            "My reply is no. ",
+            "Outlook good. ",
+            "Don't count on it."
+        };
+        #endregion
+
     }
 }
