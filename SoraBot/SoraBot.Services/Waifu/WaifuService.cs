fff04f05b1f7862ba7d25de66b0eaf7d3db178fd
Serenity <d.palbanese@gmail.com>
2020-04-13 16:14:06 +0200

    Added method to get a random waifu

--- SoraBot/SoraBot.Services/Waifu/WaifuService.cs
+++ SoraBot/SoraBot.Services/Waifu/WaifuService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using SoraBot.Data.Models.SoraDb;
 using SoraBot.Data.Repositories.Interfaces;
 using SoraBot.Services.Cache;
-
+using SoraBot.Services.Utils;
 using WaifuDbo = SoraBot.Data.Models.SoraDb.Waifu;
 
 namespace SoraBot.Services.Waifu
@@ -12,13 +14,24 @@ namespace SoraBot.Services.Waifu
     {
         private readonly ICacheService _cacheService;
         private readonly IWaifuRepository _waifuRepo;
+        private readonly RandomNumberService _rand;
 
         private const int _WAIFU_CACHE_TTL_MINS = 5;
 
-        public WaifuService(ICacheService cacheService, IWaifuRepository waifuRepo)
+        private const int _COMMON_CHANCE = 525;
+        private const int _UNCOMMON_CHANCE = 355;
+        private const int _RARE_CHANCE = 85;
+        private const int _EPIC_CHANCE = 27;
+        private const int _ULTI_CHANCE = 8;
+
+        public WaifuService(
+            ICacheService cacheService, 
+            IWaifuRepository waifuRepo,
+            RandomNumberService rand)
         {
             _cacheService = cacheService;
             _waifuRepo = waifuRepo;
+            _rand = rand;
         }
 
         /// <summary>
@@ -31,6 +44,44 @@ namespace SoraBot.Services.Waifu
             return await _cacheService.GetOrSetAndGetAsync<List<WaifuDbo>>((ulong)CustomCacheIDs.WaifuList,
                 async () => await _waifuRepo.GetAllWaifus().ConfigureAwait(false),
                 TimeSpan.FromMinutes(_WAIFU_CACHE_TTL_MINS)).ConfigureAwait(false);
+        }
+
+        public async Task<WaifuDbo> GetRandomWaifu()
+        {
+            var waifus = await this.GetAllWaifus().ConfigureAwait(false);
+            if (waifus.Count == 0) return null;
+            
+            // First we get a random rarity
+            var rarity = this.GetRandomRarity();
+            // Get list of waifus with that rarity
+            var listOfRarity = waifus.Where(x => x.Rarity == rarity).ToList();
+            // pick random waifu from that list
+            return listOfRarity[_rand.GetRandomNext(0, listOfRarity.Count)];
+
+        }
+        
+        private WaifuRarity GetRandomRarity()
+        {
+            // get a number from 0 - 999
+            int num = _rand.GetRandomNext(0, 1000);
+            var chance = _COMMON_CHANCE;
+            
+            if (num < chance)
+                return WaifuRarity.Common;
+
+            chance += _UNCOMMON_CHANCE;
+            if (num < chance)
+                return WaifuRarity.Uncommon;
+            
+            chance += _ULTI_CHANCE;
+            if (num < chance)
+                return WaifuRarity.UltimateWaifu;
+
+            chance += _RARE_CHANCE;
+            if (num < chance)
+                return WaifuRarity.Rare;
+
+            return WaifuRarity.Epic;
         }
     }
 }
