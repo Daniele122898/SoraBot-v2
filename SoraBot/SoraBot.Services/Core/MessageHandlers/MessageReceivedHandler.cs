f5281deeafc51b69f25fcf54aa833500aba54654
Serenity <d.palbanese@gmail.com>
2020-04-14 13:24:31 +0200

    Got rid of small warning

--- SoraBot/SoraBot.Services/Core/MessageHandlers/MessageReceivedHandler.cs
+++ SoraBot/SoraBot.Services/Core/MessageHandlers/MessageReceivedHandler.cs
@@ -4,7 +4,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SoraBot.Common.Messages;
 using SoraBot.Common.Messages.MessageAdapters;
