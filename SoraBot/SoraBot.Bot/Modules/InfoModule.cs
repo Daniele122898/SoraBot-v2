dc1742d11d91dbfc5d89816657fcd0fbfbb25049
Serenity <d.palbanese@gmail.com>
2020-04-18 19:59:15 +0200

    Started info module

--- SoraBot/SoraBot.Bot/Modules/InfoModule.cs
+++ SoraBot/SoraBot.Bot/Modules/InfoModule.cs
@@ -1,7 +1,29 @@
-﻿namespace SoraBot.Bot.Modules
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using SoraBot.Common.Extensions.Modules;
+
+namespace SoraBot.Bot.Modules
 {
-    public class InfoModule
+    [Name("Info")]
+    [Summary("Commands for general information about users or Sora")]
+    public class InfoModule : SoraSocketCommandModule
     {
-        
+        [Command("avatar")]
+        [Summary("Get the avatar of the @user or yourself if no one is tagged")]
+        public async Task GetAvatar(
+            [Summary("@User to get the avatar from, or no one to get your own")]
+            SocketUser userT = null)
+        {
+            var user = userT ?? Context.User;
+            var eb = new EmbedBuilder()
+            {
+                Footer  = RequestedByFooter(Context.User),
+                ImageUrl = user.GetAvatarUrl(ImageFormat.Auto, 512) ?? user.GetDefaultAvatarUrl(),
+                Color = Purple
+            };
+            await ReplyEmbed(eb);
+        }
     }
 }
