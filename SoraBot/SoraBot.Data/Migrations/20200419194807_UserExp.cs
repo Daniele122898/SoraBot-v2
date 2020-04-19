3943d45315f6432944fc0784da4dde76ae0225a4
Serenity <d.palbanese@gmail.com>
2020-04-19 22:11:59 +0200

    Added UserDb model

---
+++ SoraBot/SoraBot.Data/Migrations/20200419194807_UserExp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoraBot.Data.Migrations
+{
+    public partial class UserExp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "Exp",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Exp",
+                table: "Users");
+        }
+    }
+}
