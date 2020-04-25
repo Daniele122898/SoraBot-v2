3f8cf716c92b9a28d7dba2b0e79812b77dfc9ce0
Serenity <d.palbanese@gmail.com>
2020-04-25 22:30:48 +0200

    New function for httpclient helper

--- SoraBot/SoraBot.Services/Utils/HttpClientHelper.cs
+++ SoraBot/SoraBot.Services/Utils/HttpClientHelper.cs
@@ -20,16 +20,23 @@ namespace SoraBot.Services.Utils
         /// </summary>
         public async Task DownloadAndSaveFile(Uri url, string path)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, url);
-            using var resp = await HttpClient.SendAsync(request).ConfigureAwait(false);
+            var contentStream = await this.DownloadFileAsStream(url).ConfigureAwait(false);
             
-            await using Stream contentStream = await resp.Content.ReadAsStreamAsync().ConfigureAwait(false);
             await using var stream = new FileStream(path, FileMode.Create, 
                 FileAccess.Write, FileShare.None, 3145728, true);
             
             await contentStream.CopyToAsync(stream).ConfigureAwait(false);
             await contentStream.FlushAsync().ConfigureAwait(false);
+
             await stream.FlushAsync().ConfigureAwait(false);
+            await contentStream.DisposeAsync();
+        }
+
+        public async Task<Stream> DownloadFileAsStream(Uri url)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            using var resp = await HttpClient.SendAsync(request).ConfigureAwait(false);
+            return await resp.Content.ReadAsStreamAsync().ConfigureAwait(false);
         }
 
         public void Dispose()
