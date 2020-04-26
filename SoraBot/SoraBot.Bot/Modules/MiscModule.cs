7e77dca797ae549caeb354f6da9ff9a2939dca51
Serenity <d.palbanese@gmail.com>
2020-04-26 22:02:50 +0200

    Added misc module with invite command

--- SoraBot/SoraBot.Bot/Modules/MiscModule.cs
+++ SoraBot/SoraBot.Bot/Modules/MiscModule.cs
@@ -1,7 +1,38 @@
-﻿namespace SoraBot.Bot.Modules
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Options;
+using SoraBot.Common.Extensions.Modules;
+using SoraBot.Data.Configurations;
+
+namespace SoraBot.Bot.Modules
 {
-    public class MiscModule
+    [Name("Misc")]
+    [Summary("Miscellaneous commands")]
+    public class MiscModule : SoraSocketCommandModule
     {
-        
+        private readonly SoraBotConfig _config;
+
+        public MiscModule(IOptions<SoraBotConfig> conf)
+        {
+            _config = conf?.Value ?? throw new ArgumentNullException(nameof(conf));
+        }
+
+        [Command("inivite"), Alias("inv")]
+        [Summary("Gives you the invite link to invite Sora")]
+        public async Task InviteSora()
+            => await ReplyEmbed(new EmbedBuilder()
+            {
+                Color = Purple,
+                Title = "✉️ Invite Sora to your Guild (click me)",
+                Description = "The selected permissions are the needed permissions if you intend to use " +
+                              "ALL of Sora's features. If you dont intend to use the moderation or co you dont " +
+                              "need to give him all the perms. He just wont be able to do certain things if he misses " +
+                              "some permissions.",
+                Url = _config.SoraBotInvite,
+                ThumbnailUrl = Context.Client.CurrentUser.GetAvatarUrl(),
+                Footer = RequestedByMe()
+            });
     }
 }
