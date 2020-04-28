2ffce743bbfce95734ddb9c729a2ed6411c1e14b
Serenity <d.palbanese@gmail.com>
2020-04-28 22:48:43 +0200

    Added ReactionEventWrapper

---
+++ SoraBot/SoraBot.Common/Messages/MessageAdapters/ReactionReceived.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace SoraBot.Common.Messages.MessageAdapters
+{
+    public enum ReactionEventType
+    {
+        Added,
+        Removed,
+        Cleared
+    }
+    
+    public class ReactionReceived : IMessage
+    {
+        public Cacheable<IUserMessage, ulong> Message { get; private set; }
+        public ISocketMessageChannel Channel { get; private set; }
+        public SocketReaction Reaction { get; private set; }
+        public ReactionEventType Type { get; private set; }
+
+        public ReactionReceived(
+            ReactionEventType type, 
+            Cacheable<IUserMessage, ulong> message, 
+            ISocketMessageChannel channel,
+            SocketReaction reaction = null)
+        {
+            this.Type = type;
+            this.Message = message;
+            this.Channel = channel;
+            this.Reaction = reaction;
+        }
+    }
+}
