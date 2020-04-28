4941638c227e7f86c9656cb0809c09ab8918fe10
Serenity <d.palbanese@gmail.com>
2020-04-29 01:01:16 +0200

    #113 - Added repository methods to edit starboard info

--- SoraBot/SoraBot.Data/Repositories/Interfaces/IStarboardRepository.cs
+++ SoraBot/SoraBot.Data/Repositories/Interfaces/IStarboardRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace SoraBot.Data.Repositories.Interfaces
+﻿using System.Threading.Tasks;
+using ArgonautCore.Maybe;
+
+namespace SoraBot.Data.Repositories.Interfaces
 {
     public interface IStarboardRepository
     {
-        
+        Task<Maybe<(ulong starboardChannelId, uint threshold)>> GetStarboardInfo(ulong guildId);
+        Task SetStarboardChannleId(ulong guildId, ulong? starboardChannelId);
+        Task SetStarboardThreshold(ulong guildId, uint threshold);
     }
 }
--- SoraBot/SoraBot.Data/Repositories/StarboardRepository.cs
+++ SoraBot/SoraBot.Data/Repositories/StarboardRepository.cs
@@ -1,9 +1,44 @@
-﻿using SoraBot.Data.Repositories.Interfaces;
+﻿using System.Threading.Tasks;
+using ArgonautCore.Maybe;
+using SoraBot.Data.Repositories.Interfaces;
 
 namespace SoraBot.Data.Repositories
 {
     public class StarboardRepository : IStarboardRepository
     {
+        private readonly ITransactor<SoraContext> _soraTransactor;
+
+        public StarboardRepository(ITransactor<SoraContext> soraTransactor)
+        {
+            _soraTransactor = soraTransactor;
+        }
         
+        public async Task<Maybe<(ulong starboardChannelId, uint threshold)>> GetStarboardInfo(ulong guildId)
+        {
+            return await _soraTransactor.DoAsync<Maybe<(ulong, uint)>>(async context =>
+            {
+                var guild = await context.Guilds.FindAsync(guildId).ConfigureAwait(false);
+                if (guild?.StarboardChannelId == null) return Maybe.Zero<(ulong, uint)>();
+                return Maybe.FromVal<(ulong, uint)>((guild.StarboardChannelId.Value, guild.StarboardThreshold));
+            }).ConfigureAwait(false);
+        }
+
+        public async Task SetStarboardChannleId(ulong guildId, ulong? starboardChannelId)
+            => await _soraTransactor.DoInTransactionAsync(async context =>
+            {
+                var guild = await context.Guilds.FindAsync(guildId).ConfigureAwait(false);
+                if (guild == null) return;
+                guild.StarboardChannelId = starboardChannelId;
+                await context.SaveChangesAsync();
+            }).ConfigureAwait(false);
+
+        public async Task SetStarboardThreshold(ulong guildId, uint threshold)
+            => await _soraTransactor.DoInTransactionAsync(async context =>
+            {
+                var guild = await context.Guilds.FindAsync(guildId).ConfigureAwait(false);
+                if (guild == null) return;
+                guild.StarboardThreshold = threshold;
+                await context.SaveChangesAsync();
+            }).ConfigureAwait(false);
     }
 }
