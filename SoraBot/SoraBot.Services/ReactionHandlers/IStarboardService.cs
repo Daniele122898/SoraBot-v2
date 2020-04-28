7e9bdd3d9b6428d6905741a7553bd277905eeb9b
Serenity <d.palbanese@gmail.com>
2020-04-28 23:09:53 +0200

    #113 Reaction starboard handlers

--- SoraBot/SoraBot.Services/ReactionHandlers/IStarboardService.cs
+++ SoraBot/SoraBot.Services/ReactionHandlers/IStarboardService.cs
@@ -1,7 +1,18 @@
-﻿namespace SoraBot.Services.ReactionHandlers
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using SoraBot.Common.Messages.MessageAdapters;
+
+namespace SoraBot.Services.ReactionHandlers
 {
     public interface IStarboardService
     {
+        Task HandleReactionAdded(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel,
+            SocketReaction reaction);
         
+        Task HandleReactionRemoved(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel,
+            SocketReaction reaction);
+        
+        Task HandleReactionCleared(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel);
     }
 }
