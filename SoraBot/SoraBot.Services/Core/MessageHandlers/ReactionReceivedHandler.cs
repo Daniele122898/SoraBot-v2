70ae76e8ff206e875af68834b462246ec5f49933
Serenity <d.palbanese@gmail.com>
2020-04-28 23:02:13 +0200

    #113 Stump for reaction Handler

--- SoraBot/SoraBot.Services/Core/MessageHandlers/ReactionReceivedHandler.cs
+++ SoraBot/SoraBot.Services/Core/MessageHandlers/ReactionReceivedHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using SoraBot.Common.Messages;
 using SoraBot.Common.Messages.MessageAdapters;
@@ -7,9 +8,37 @@ namespace SoraBot.Services.Core.MessageHandlers
 {
     public class ReactionReceivedHandler : IMessageHandler<ReactionReceived>
     {
-        public Task HandleMessageAsync(ReactionReceived message, CancellationToken cancellationToken = default)
+        public async Task HandleMessageAsync(ReactionReceived reaction, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            switch (reaction.Type)
+            {
+                case ReactionEventType.Added:
+                    await this.HandleReactionAdded(reaction).ConfigureAwait(false);
+                    break;
+                case ReactionEventType.Removed:
+                    await this.HandleReactionRemoved(reaction).ConfigureAwait(false);
+                    break;
+                case ReactionEventType.Cleared:
+                    await this.HandleReactionCleared(reaction).ConfigureAwait(false);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private async Task HandleReactionAdded(ReactionReceived reaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task HandleReactionRemoved(ReactionReceived reaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task HandleReactionCleared(ReactionReceived reaction)
+        {
+            throw new NotImplementedException();
         }
     }
 }
