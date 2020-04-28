02e79e788372f0054d24b875f4283c51a8eb95d1
Serenity <d.palbanese@gmail.com>
2020-04-28 23:33:44 +0200

    #113 - First sanity checks on starboard reaction received

--- SoraBot/SoraBot.Services/ReactionHandlers/StarboardService.cs
+++ SoraBot/SoraBot.Services/ReactionHandlers/StarboardService.cs
@@ -6,19 +6,27 @@ namespace SoraBot.Services.ReactionHandlers
 {
     public class StarboardService : IStarboardService
     {
+        private static bool IsStarEmote(IEmote emote)
+            => emote.Name == "⭐";
         
-        
-        public Task HandleReactionAdded(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel, SocketReaction reaction)
+        public async Task HandleReactionAdded(Cacheable<IUserMessage, ulong> msg, SocketReaction reaction)
+        {
+            if (!IsStarEmote(reaction.Emote)) return;
+            // Try get message
+            var message = await msg.GetOrDownloadAsync().ConfigureAwait(false);
+            if (message == null) return;
+            // Check if this is in a guild and not DMs
+            if (!(message.Channel is IGuildChannel channel)) return;
+                        
+            
+        }
+
+        public Task HandleReactionRemoved(Cacheable<IUserMessage, ulong> msg, SocketReaction reaction)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task HandleReactionRemoved(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel, SocketReaction reaction)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task HandleReactionCleared(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel)
+        public Task HandleReactionCleared(Cacheable<IUserMessage, ulong> msg)
         {
             throw new System.NotImplementedException();
         }
