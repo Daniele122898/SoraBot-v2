d4aba06ea17ad24763255e7d2ede0d5a35076840
Serenity <d.palbanese@gmail.com>
2020-04-29 23:34:20 +0200

    #113 - Refactored url helper out

--- SoraBot/SoraBot.Common/Utils/Helper.cs
+++ SoraBot/SoraBot.Common/Utils/Helper.cs
@@ -7,5 +7,11 @@ namespace SoraBot.Common.Utils
         public static bool UrlValidUri(string url)
             => Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        
+        public static bool LinkIsNoImage(string url)
+            => !url.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) &&
+               !url.EndsWith(".png", StringComparison.OrdinalIgnoreCase) &&
+               !url.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) &&
+               !url.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase);
     }
 }
