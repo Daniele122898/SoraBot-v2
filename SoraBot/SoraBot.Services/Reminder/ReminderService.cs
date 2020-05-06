7e9cc9d22ed0c79b46e9365fa6d73aac645e2af0
Serenity <d.palbanese@gmail.com>
2020-05-06 21:21:45 +0200

    #111 - Introduced the timers

--- SoraBot/SoraBot.Services/Reminder/ReminderService.cs
+++ SoraBot/SoraBot.Services/Reminder/ReminderService.cs
@@ -1,7 +1,42 @@
-﻿namespace SoraBot.Services.Reminder
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using SoraBot.Services.Utils;
+
+namespace SoraBot.Services.Reminder
 {
     public class ReminderService : IReminderService
     {
+        private readonly ILogger<ReminderService> _log;
+        public const int TIMER_INTERVAL_MINS = 1;
         
+        private Timer _timer;
+        
+        public ReminderService(ILogger<ReminderService> log)
+        {
+            _log = log;
+            _timer = new Timer(CheckReminders, null, TimeSpan.FromMinutes(TIMER_INTERVAL_MINS), 
+                TimeSpan.FromMinutes(TIMER_INTERVAL_MINS));
+        }
+
+        private void CheckReminders(object _)
+        {
+            // Change it since this might take a while or introduce some kind of lag
+            // so we dont accidentally want to have multiple instances running at the same time
+            _timer.Change(TimeSpan.FromHours(1), TimeSpan.FromHours(1));
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                _log.LogError(e, "Failed to send or manipulate reminders!");
+            }
+            finally
+            {
+                _timer.Change(TimeSpan.FromMinutes(TIMER_INTERVAL_MINS),
+                    TimeSpan.FromMinutes(TIMER_INTERVAL_MINS));
+            }
+        }
     }
 }
