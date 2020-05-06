cfed347681635c67f479510b3b71fd6c951bbc56
Serenity <d.palbanese@gmail.com>
2020-05-06 23:14:34 +0200

    #111 - Made userserivce singleton bcs it didnt need to be scoped

--- SoraBot/SoraBot.Services/Users/UserServicesDependencyInjection.cs
+++ SoraBot/SoraBot.Services/Users/UserServicesDependencyInjection.cs
@@ -5,6 +5,6 @@ namespace SoraBot.Services.Users
     public static class UserServicesDependencyInjection
     {
         public static IServiceCollection AddUserServices(this IServiceCollection services)
-            => services.AddScoped<IUserService, UserService>();
+            => services.AddSingleton<IUserService, UserService>();
     }
 }
