c73b614a5e2fb1720346e0a5b64be299ad319821
Serenity <d.palbanese@gmail.com>
2020-05-09 17:59:50 +0200

    #110 - Added needed migrations

---
+++ SoraBot/SoraBot.Data/Migrations/20200509155916_Sar.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoraBot.Data.Migrations
+{
+    public partial class Sar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sars",
+                columns: table => new
+                {
+                    RoleId = table.Column<ulong>(nullable: false),
+                    GuildId = table.Column<ulong>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sars", x => x.RoleId);
+                    table.ForeignKey(
+                        name: "FK_Sars_Guilds_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guilds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sars_GuildId",
+                table: "Sars",
+                column: "GuildId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sars");
+        }
+    }
+}
