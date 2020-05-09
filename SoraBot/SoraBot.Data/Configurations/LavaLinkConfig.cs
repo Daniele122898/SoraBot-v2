38efb804ebf40b8b4d848799da4a38d24a879691
Serenity <d.palbanese@gmail.com>
2020-05-09 20:30:18 +0200

    #115 - Added Victoria bcs there's nothing else :(

---
+++ SoraBot/SoraBot.Data/Configurations/LavaLinkConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace SoraBot.Data.Configurations
+{
+    public class LavaLinkConfig
+    {
+        public string Password { get; set; }
+        public ushort Port { get; set; }
+        public string IP { get; set; }
+    }
+}
