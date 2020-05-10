725332314c4bf906d71929ccd0b8f712ddd4e253
Serenity <d.palbanese@gmail.com>
2020-05-10 11:48:40 +0200

    #115 - Added logging

--- SoraBot/SoraBot.Bot/Modules/AudioModule/AudioEventHandler.cs
+++ SoraBot/SoraBot.Bot/Modules/AudioModule/AudioEventHandler.cs
@@ -1,15 +1,55 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Microsoft.Extensions.Logging;
+using Victoria;
 
 namespace SoraBot.Bot.Modules.AudioModule
 {
-    public class AudioEventHandler
+    public class AudioEventHandler : IDisposable
     {
         private readonly ILogger<AudioEventHandler> _log;
+        private readonly LavaNode _node;
 
-        public AudioEventHandler(ILogger<AudioEventHandler> log)
+        public AudioEventHandler(ILogger<AudioEventHandler> log, LavaNode node)
         {
             log.LogInformation("Initialized Audio Event Handlers");
             _log = log;
+            _node = node;
+            
+            _node.OnLog += OnLog;
+        }
+
+        private Task OnLog(LogMessage log)
+        {
+            switch (log.Severity)
+            {
+                case LogSeverity.Critical:
+                    _log.LogWarning(log.Exception, log.Message);
+                    break;
+                case LogSeverity.Error:
+                    _log.LogWarning(log.Exception, log.Message);
+                    break;
+                case LogSeverity.Warning:
+                    _log.LogWarning(log.Exception, log.Message);
+                    break;
+                case LogSeverity.Info:
+                    _log.LogInformation(log.Message);
+                    break;
+                case LogSeverity.Verbose:
+                    _log.LogTrace(log.Message);
+                    break;
+                case LogSeverity.Debug:
+                    _log.LogDebug(log.Message);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
         }
     }
 }
