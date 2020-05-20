00f18104e2d490009ad7637825b07c9e6d2aa7e6
Serenity <d.palbanese@gmail.com>
2020-05-20 23:18:33 +0200

    #116 - Implemented guild leaderboard endpoint

--- SoraBot/SoraBot.WebApi/Controllers/LeaderboardController.cs
+++ SoraBot/SoraBot.WebApi/Controllers/LeaderboardController.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
 using Microsoft.AspNetCore.Mvc;
 using SoraBot.Data.Repositories.Interfaces;
 using SoraBot.Services.Users;
@@ -13,17 +18,60 @@ namespace SoraBot.WebApi.Controllers
     {
         private readonly IProfileRepository _profileRepo;
         private readonly IUserService _userService;
+        private readonly DiscordSocketClient _client;
+        private readonly DiscordRestClient _restClient;
 
-        public LeaderboardController(IProfileRepository profileRepo, IUserService userService)
+        public LeaderboardController(
+            IProfileRepository profileRepo, 
+            IUserService userService,
+            DiscordSocketClient client,
+            DiscordRestClient restClient)
         {
             _profileRepo = profileRepo;
             _userService = userService;
+            _client = client;
+            _restClient = restClient;
         }
 
         [HttpGet("{guildId}")]
         public async Task<IActionResult> GetLocalLeaderboard(ulong guildId)
         {
-            
+            var guild = _client.GetGuild(guildId);
+            if (guild == null)
+                return NotFound("The specified guild could not be found!");
+            var gl = new GuildLeaderboard()
+            {
+                AvatarUrl = guild.IconUrl ?? _client.CurrentUser.GetAvatarUrl(),
+                GuildName = guild.Name,
+                Ranks = new List<GuildRank>(guild.MemberCount > 100 ? 100: guild.MemberCount) // Properly set capacity already for best performance!
+            };
+
+            var gUsersO = await _profileRepo.GetGuildUsersSorted(guildId);
+            if (!gUsersO)
+                return NotFound("Guild has no saved users!");
+
+            var g = await _restClient.GetGuildAsync(guildId);
+            // Create dictionary for easy and fast lookups later :)
+            var users = (await g.GetUsersAsync().FlattenAsync()).ToDictionary(x=> x.Id, x=> x);
+            var gUsers = (~gUsersO);
+            for (var i = 0; i < gUsers.Count; i++)
+            {
+                var user = gUsers[i];
+                int rank = i + 1;
+                if (rank > 100) break;
+                if (!users.TryGetValue(user.UserId, out var u))
+                    continue;
+                gl.Ranks.Add(new GuildRank()
+                {
+                    AvatarUrl = u.GetAvatarUrl() ?? u.GetDefaultAvatarUrl(),
+                    Discrim = u.Discriminator,
+                    Exp = user.Exp,
+                    Name = u.Username,
+                    Rank = rank
+                });
+            }
+
+            return Ok(gl);
         }
 
         [HttpGet("global")]
