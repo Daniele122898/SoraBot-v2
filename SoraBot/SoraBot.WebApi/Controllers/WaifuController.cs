9e53f4564740f8da507f5ac641952cf8f442ae3d
Serenity <d.palbanese@gmail.com>
2020-05-20 23:30:38 +0200

    #116 - Removed cors

--- SoraBot/SoraBot.WebApi/Controllers/WaifuController.cs
+++ SoraBot/SoraBot.WebApi/Controllers/WaifuController.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SoraBot.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [EnableCors("AllowLocal")]
     public class WaifuController : ControllerBase
     {
         
