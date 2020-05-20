00cf4e724ffad326a2b81cb8b77829ca1f48e15a
Serenity <d.palbanese@gmail.com>
2020-05-20 23:46:22 +0200

    #116 - Added get all waifus endpoint

--- SoraBot/SoraBot.WebApi/Controllers/WaifuController.cs
+++ SoraBot/SoraBot.WebApi/Controllers/WaifuController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SoraBot.Data.Models.SoraDb;
+using SoraBot.Services.Waifu;
 
 namespace SoraBot.WebApi.Controllers
 {
@@ -6,6 +10,23 @@ namespace SoraBot.WebApi.Controllers
     [ApiController]
     public class WaifuController : ControllerBase
     {
-        
+        private readonly IWaifuService _waifuService;
+
+        // TODO this is quite heavy. might want to get a lighter way to fetch all waifus.
+        public WaifuController(IWaifuService waifuService)
+        {
+            _waifuService = waifuService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Waifu>>> GetAllWaifus()
+        {
+            var waifus = await _waifuService.GetAllWaifus().ConfigureAwait(false);
+            if (waifus == null || waifus.Count == 0)
+                return NotFound("No Waifus found");
+            // Sort in-place by rarity descending
+            waifus.Sort((x,y) => -x.Rarity.CompareTo(y.Rarity));
+            return Ok(waifus);
+        }
     }
 }
