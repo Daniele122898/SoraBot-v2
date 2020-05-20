1a2c8168fb805e17ab553155e26aecbc083a2759
Serenity <d.palbanese@gmail.com>
2020-05-20 23:18:05 +0200

    #116 - Delay list initialization so we dont move ANY memory!

--- SoraBot/SoraBot.WebApi/Dtos/GuildLeaderboard.cs
+++ SoraBot/SoraBot.WebApi/Dtos/GuildLeaderboard.cs
@@ -4,10 +4,9 @@ namespace SoraBot.WebApi.Dtos
 {
     public class GuildLeaderboard
     {
-        public bool Success { get; set; }
         public string AvatarUrl { get; set; }
         public string GuildName { get; set; }
-        public List<GuildRank> Ranks { get; set; } = new List<GuildRank>();
+        public List<GuildRank> Ranks { get; set; }
         public List<RoleReward> RoleRewards { get; set; } = new List<RoleReward>();
     }
 
