5366ab94a37a895462fe2c1111665a8b5b01f747
Serenity <d.palbanese@gmail.com>
2020-05-23 23:37:40 +0200

    #116 - Implement get all user waifus endpoint

--- SoraBot/SoraBot.WebApi/Controllers/WaifuController.cs
+++ SoraBot/SoraBot.WebApi/Controllers/WaifuController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SoraBot.Data.Models.SoraDb;
+using SoraBot.Services.Users;
 using SoraBot.Services.Waifu;
+using SoraBot.WebApi.Dtos;
 
 namespace SoraBot.WebApi.Controllers
 {
@@ -11,11 +14,13 @@ namespace SoraBot.WebApi.Controllers
     public class WaifuController : ControllerBase
     {
         private readonly IWaifuService _waifuService;
+        private readonly IUserService _userService;
 
         // TODO this is quite heavy. might want to get a lighter way to fetch all waifus.
-        public WaifuController(IWaifuService waifuService)
+        public WaifuController(IWaifuService waifuService, IUserService userService)
         {
             _waifuService = waifuService;
+            _userService = userService;
         }
 
         [HttpGet]
@@ -27,6 +32,53 @@ namespace SoraBot.WebApi.Controllers
             // Sort in-place by rarity descending
             waifus.Sort((x,y) => -x.Rarity.CompareTo(y.Rarity));
             return Ok(waifus);
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<ActionResult<UserWaifusDto>> GetAllUserWaifus(ulong userId)
+        {
+            var user = await _userService.GetOrSetAndGet(userId);
+            if (!user)
+                return NotFound("User could not be found!");
+
+            var userWaifus = await _waifuService.GetAllUserWaifus(userId);
+            if (userWaifus == null || userWaifus.Count == 0)
+                return NotFound("User has no waifus");
+            
+            var userWaifusDto = new UserWaifusDto()
+            {
+                AvatarUrl = (~user).GetAvatarUrl() ?? (~user).GetDefaultAvatarUrl(),
+                Username = (~user).Username,
+                Waifus = new List<UserWaifuDto>(userWaifus.Count) // efficient initialization of list
+            };
+
+            // Now we get the entire waifu list for a couple of reasons.
+            // We dont want to query EVERY SINGLE userwaifu bcs that's gonna be slow
+            // The entire waifu list has a high probability of being in the cache
+            // which would get rid of all the requests anyway, and if not we populate it
+            // which is nice in it's own right :)
+            // Also convert it to a dictionary for quick access later on
+            var waifus = (await _waifuService.GetAllWaifus())
+                .ToDictionary(x=> x.Id, x => x);
+
+            for (var i = 0; i < userWaifus.Count; i++)
+            {
+                var userWaifu = userWaifus[i];
+                if (!waifus.TryGetValue(userWaifu.WaifuId, out var w))
+                    continue;
+                userWaifusDto.Waifus.Add(new UserWaifuDto()
+                {
+                    Count = userWaifu.Count,
+                    Id = w.Id,
+                    ImageUrl = w.ImageUrl,
+                    Name = w.Name,
+                    Rarity = w.Rarity
+                });
+            }
+            
+            // Sort it by rarity descending
+            userWaifusDto.Waifus.Sort((x,y) => -x.Rarity.CompareTo(y.Rarity));
+            return Ok(userWaifusDto);
         }
     }
 }
---
+++ SoraBot/SoraBot.WebApi/Dtos/UserWaifusDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using SoraBot.Data.Models.SoraDb;
+
+namespace SoraBot.WebApi.Dtos
+{
+    public class UserWaifusDto
+    {
+        public string Username { get; set; }
+        public string AvatarUrl { get; set; }
+        public List<UserWaifuDto> Waifus { get; set; }
+    }
+
+    public class UserWaifuDto
+    {
+        public int Id { get; set; }
+        public uint Count { get; set; }
+        public WaifuRarity Rarity { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
