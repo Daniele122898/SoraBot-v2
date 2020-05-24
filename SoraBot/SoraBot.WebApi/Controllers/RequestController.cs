c14dac58fb71598116912add0e7abf8c9016cd9f
Serenity <d.palbanese@gmail.com>
2020-05-24 13:37:25 +0200

    #116 - Added method to update request notification

--- SoraBot/SoraBot.WebApi/Controllers/RequestController.cs
+++ SoraBot/SoraBot.WebApi/Controllers/RequestController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SoraBot.Data.Repositories.Interfaces;
+using SoraBot.Services.Users;
 
 namespace SoraBot.WebApi.Controllers
 {
@@ -6,11 +9,38 @@ namespace SoraBot.WebApi.Controllers
     [ApiController]
     public class RequestController : ControllerBase
     {
-        public RequestController()
+        private readonly IWaifuRequestRepository _waifuRequestRepo;
+        private readonly IUserService _userService;
+        private readonly IUserRepository _userRepo;
+
+        public RequestController(
+            IWaifuRequestRepository waifuRequestRepo,
+            IUserService userService,
+            IUserRepository userRepo)
         {
-            
+            _waifuRequestRepo = waifuRequestRepo;
+            _userService = userService;
+            _userRepo = userRepo;
         }
-        
-        
+
+        [HttpPost("user/{userId}/notify")]
+        public async Task<IActionResult> SetUserNotify(ulong userId, bool notify)
+        {
+            // Check if user exists
+            var user = await _userService.GetOrSetAndGet(userId);
+            if (!user)
+                return NotFound("User doesn't exist in Sora's reach");
+
+            // Make sure he exists in DB
+            await _userRepo.GetOrCreateUser(userId);
+            
+            // Now we set the notification
+            if (notify)
+                await _waifuRequestRepo.ActivateUserNotification(userId);
+            else
+                await _waifuRequestRepo.RemoveUserNotification(userId);
+            
+            return Ok();
+        }
     }
 }
