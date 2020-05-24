37165df7cb01d3f713db42a9daf8992276dd8c9b
Serenity <d.palbanese@gmail.com>
2020-05-24 12:08:03 +0200

    #116 - Added waifu request controller stump

---
+++ SoraBot/SoraBot.WebApi/Controllers/RequestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoraBot.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        public RequestController()
+        {
+            
+        }
+        
+        
+    }
+}
