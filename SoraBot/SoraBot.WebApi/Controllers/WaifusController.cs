26928bf48c3b087208d33ad35b83acf2fd82e892
Serenity <d.palbanese@gmail.com>
2020-05-25 21:08:53 +0200

    #116 - Got rid of useless import

--- SoraBot/SoraBot.WebApi/Controllers/WaifusController.cs
+++ SoraBot/SoraBot.WebApi/Controllers/WaifusController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Extensions;
 using SoraBot.Data.Models.SoraDb;
 using SoraBot.Services.Users;
 using SoraBot.Services.Waifu;
