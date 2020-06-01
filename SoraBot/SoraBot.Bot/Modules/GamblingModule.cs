8e3f39c497bbde9c9c44c0e531ff42beb12f51c6
Serenity <d.palbanese@gmail.com>
2020-06-01 11:57:20 +0200

    Add gambling stump

---
+++ SoraBot/SoraBot.Bot/Modules/GamblingModule.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+using SoraBot.Common.Extensions.Modules;
+
+namespace SoraBot.Bot.Modules
+{
+    [Name("Gambling")]
+    [Summary("All commands around gambling and loosing all your Sora Coins ;)")]
+    public class GamblingModule : SoraSocketCommandModule
+    {
+        
+    }
+}
