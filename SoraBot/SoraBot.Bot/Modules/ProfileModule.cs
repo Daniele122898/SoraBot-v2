2f6cc044e758d28e41d5afc453209af2ea7d863c
Serenity <d.palbanese@gmail.com>
2020-06-01 11:54:10 +0200

    Reduce error reporting that we dont care about

--- SoraBot/SoraBot.Bot/Modules/ProfileModule.cs
+++ SoraBot/SoraBot.Bot/Modules/ProfileModule.cs
@@ -175,6 +175,8 @@ namespace SoraBot.Bot.Modules
                     "This could have multiple reasons. One of them could be that your username has characters that " +
                     "are currently not supported. This is any weird character that you wouldn't naturally find on your standard " +
                     "keyboard.");
+                if (e.InnerException is NotImplementedException)
+                    return; // Since we dont care about exceptions about not supported text and shit. Can't do anything about that
                 _log.LogError(e, $"Failed to generate image for {user.Id.ToString()} ({user.Username})");
             }
             finally
