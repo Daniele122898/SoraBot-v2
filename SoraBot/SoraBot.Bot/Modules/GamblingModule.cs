bea161aabbe6a738115f5e225be6a71cffd5b3db
Serenity <d.palbanese@gmail.com>
2020-06-01 13:38:20 +0200

    Added coinflip

--- SoraBot/SoraBot.Bot/Modules/GamblingModule.cs
+++ SoraBot/SoraBot.Bot/Modules/GamblingModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord.Commands;
 using SoraBot.Common.Extensions.Modules;
 using SoraBot.Data.Repositories.Interfaces;
@@ -57,6 +58,50 @@ namespace SoraBot.Bot.Modules
                 await ReplyMoneyEmbed($"Congratulations! You won {(bet * 6).ToString()} Sora Coins!");
             else
                 await ReplyMoneyLostEmbed($"You lost :( The dice was {roll.ToString()}. Better luck next time!");
+        }
+
+        [Command("coinflip"), Alias("cf", "flip")]
+        [Summary("Flip a coin and bet on a side! If you win you'll get double your bet")]
+        public async Task FlipCoin(int bet, string side)
+        {
+            if (bet <= 0)
+            {
+                await ReplyFailureEmbed("Please bet an amount greater than 0!");
+                return;
+            }
+            side = side.Trim();
+
+            if (!side.Equals("heads", StringComparison.OrdinalIgnoreCase) &&
+                !side.Equals("tails", StringComparison.OrdinalIgnoreCase))
+            {
+                await ReplyFailureEmbed("Please choose between `heads` or `tails` only.");
+                return;
+            }
+            
+            var availableCoins = _coinRepository.GetCoins(Context.User.Id);
+            if (bet > availableCoins)
+            {
+                await ReplyFailureEmbed(
+                    $"Please specify a bet smaller or equal to your total Sora coin amount ({availableCoins.ToString()} SC).");
+                return;
+            }
+
+            // Instead of choosing a side we just give the user a 50% chance to win. 
+            // Since mathematically speaking its the same. We then decide on the side
+            // based on the fact if he won or lost.
+            bool win = _rand.GetRandomNext(100) % 2 == 0;
+            if (win)
+                await _coinRepository.GiveAmount(Context.User.Id, (uint) (bet));
+            else if (!await _coinRepository.TryTakeAmount(Context.User.Id, (uint) bet))
+            {
+                await ReplyFailureEmbed("You didn't have enough Sora coins for the transfer. Try again");
+                return;
+            }
+            
+            if (win)
+                await ReplyMoneyEmbed($"Congratulations! You won {(bet * 2).ToString()} Sora Coins!");
+            else
+                await ReplyMoneyLostEmbed($"You lost :( Better luck next time!");
         }
     }
 }
