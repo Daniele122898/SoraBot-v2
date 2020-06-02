056844a3316d219688ed8279d3dfe9f6722a9ab0
Serenity <d.palbanese@gmail.com>
2020-06-02 21:19:26 +0200

    #133 - Added needed migrations

---
+++ SoraBot/SoraBot.Data/Migrations/20200602191216_RejectReason.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoraBot.Data.Migrations
+{
+    public partial class RejectReason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RejectReason",
+                table: "WaifuRequests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectReason",
+                table: "WaifuRequests");
+        }
+    }
+}
