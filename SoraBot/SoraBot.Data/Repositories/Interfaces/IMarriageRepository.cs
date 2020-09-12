45426f087818200c1aebe0de31cf8ef86fb4d36a
Serenity <d.palbanese@gmail.com>
2020-09-12 15:45:09 +0200

    #112 - Added repository stump

---
+++ SoraBot/SoraBot.Data/Repositories/Interfaces/IMarriageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ArgonautCore.Lw;
+using SoraBot.Data.Models.SoraDb;
+
+namespace SoraBot.Data.Repositories.Interfaces
+{
+    public interface IMarriageRepository
+    {
+        public Task<Option<List<Marriage>>> GetAllMarriagesOfUser(ulong userId);
+        public Task<bool> TryAddMarriage(ulong user1, ulong user2);
+        public Task<bool> TryDivorce(ulong user1, ulong user2);
+
+    }
+}
---
+++ SoraBot/SoraBot.Data/Repositories/MarriageRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArgonautCore.Lw;
+using Microsoft.EntityFrameworkCore;
+using SoraBot.Data.Models.SoraDb;
+using SoraBot.Data.Repositories.Interfaces;
+
+namespace SoraBot.Data.Repositories
+{
+    public class MarriageRepository : IMarriageRepository
+    {
+        private readonly ITransactor<SoraContext> _soraTransactor;
+
+        public MarriageRepository(ITransactor<SoraContext> soraTransactor)
+        {
+            _soraTransactor = soraTransactor;
+        }
+
+        public async Task<Option<List<Marriage>>> GetAllMarriagesOfUser(ulong userId)
+            => await _soraTransactor.DoAsync(async context =>
+            {
+                var marriages = await context.Marriages.Where(x => x.Partner1 == userId || x.Partner2 == userId)
+                    .ToListAsync().ConfigureAwait(false);
+
+                if (marriages.Count == 0)
+                    return Option.None<List<Marriage>>();
+                
+                return marriages;
+            }).ConfigureAwait(false);
+
+        public Task<bool> TryAddMarriage(ulong user1, ulong user2)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> TryDivorce(ulong user1, ulong user2)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
