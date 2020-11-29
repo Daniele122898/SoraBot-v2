0301f483c2fb51b8588bf0637e3af662630a82e4
Serenity <d.palbanese@gmail.com>
2020-11-29 22:18:19 +0100

    #108 - Add initial interface stumps

--- SoraBot/SoraBot.Data/Repositories/Interfaces/IClanRepository.cs
+++ SoraBot/SoraBot.Data/Repositories/Interfaces/IClanRepository.cs
@@ -1,7 +1,18 @@
-﻿namespace SoraBot.Data.Repositories.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ArgonautCore.Lw;
+using SoraBot.Data.Models.SoraDb;
+
+namespace SoraBot.Data.Repositories.Interfaces
 {
     public interface IClanRepository
     {
+        public Task<Option<Clan>> GetClanById(int clanId);
         
+        public Task<Option<Clan>> GetClanByName(string clanName);
+
+        public Task<Option<List<User>>> GetClanMembers(int clanId);
+
+        public Task<int> GetMemberCount(int clanId);
     }
 }
