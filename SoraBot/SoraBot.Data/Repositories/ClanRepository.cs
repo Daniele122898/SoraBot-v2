3db582cc91e5a0e72be2f3c878afd49c9f718656
Serenity <d.palbanese@gmail.com>
2020-11-29 22:08:28 +0100

    #108 - Add Repository stumps

---
+++ SoraBot/SoraBot.Data/Repositories/ClanRepository.cs
@@ -0,0 +1,14 @@
+﻿using SoraBot.Data.Repositories.Interfaces;
+
+namespace SoraBot.Data.Repositories
+{
+    public class ClanRepository : IClanRepository
+    {
+        private readonly ITransactor<SoraContext> _soraTransactor;
+        
+        public ClanRepository(ITransactor<SoraContext> soraTransactor)
+        {
+            _soraTransactor = soraTransactor;
+        }
+    }
+}
