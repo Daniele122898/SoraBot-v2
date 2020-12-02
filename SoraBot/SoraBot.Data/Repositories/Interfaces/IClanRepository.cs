458702fca5dd97e5c722972b9b0d60d06e75f86e
Serenity <d.palbanese@gmail.com>
2020-12-02 23:39:56 +0100

    #108 - More repo stumps

--- SoraBot/SoraBot.Data/Repositories/Interfaces/IClanRepository.cs
+++ SoraBot/SoraBot.Data/Repositories/Interfaces/IClanRepository.cs
@@ -8,11 +8,16 @@ namespace SoraBot.Data.Repositories.Interfaces
     public interface IClanRepository
     {
         public Task<Option<Clan>> GetClanById(int clanId);
-        
         public Task<Option<Clan>> GetClanByName(string clanName);
-
         public Task<Option<List<User>>> GetClanMembers(int clanId);
-
         public Task<int> GetMemberCount(int clanId);
+        public Task<bool> DoesClanExistByName(string name);
+        public Task<Option<Clan>> GetClanByUserId(ulong userId);
+        public Task CreateClan(string name);
+        public Task SetClanDescription(string clanId, string description);
+        public Task SetClanAvatar(string avatarUrl);
+        public Task LevelUp(int clanId);
+        public Task ChangeClanName(int clanId, string newName);
+
     }
 }
